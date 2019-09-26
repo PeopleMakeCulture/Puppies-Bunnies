a26135ca4ecdaae8bf2ffcf406b3216abdcb07bf
Allison Geismar <ageismar213@gmail.com>
2019-09-26 18:53:35 -0400

    changed the name and set up the models folder

--- models/index.js
+++ models/index.js
@@ -2,35 +2,17 @@ const Sequelize = require('sequelize');
 // const db = new Sequelize('postgres://localhost:5432/wikistack');
 
 //or id we do not want to see  the console output (SQL command text for each query) the we do this =>
-const db = new Sequelize('postgres://localhost:5432/wikistack', {
+const db = new Sequelize('postgres://localhost:5432/Bunnies-Puppies', {
     logging: false
 });
 
 module.exports = {
   db
 }
-const Page = db.define('page', {
-    title: {
-      type: Sequelize.STRING, allowNull: false
-    },
-    slug: {
-      type: Sequelize.STRING, allowNull: false
-    },
-    content: {
-      type: Sequelize.TEXT, allowNull: false
-    },
-    status: {
-      type: Sequelize.ENUM('open', 'closed')
-    }
-  });
+//create models for puppies 
+  //include: name(string), age(int), breed(ENUM), isGoodBoy(Boolean) with default to true
 
-  const User = db.define('user', {
-    name: {
-      type: Sequelize.STRING, allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING, allowNull: false
-    }
-  });
+//create models for bunnies
+  //include: name(string), age(int), floppyEars(boolean)
 
-  module.eports = { Page, User };
+  module.eports = { Bunnies, Puppies };
