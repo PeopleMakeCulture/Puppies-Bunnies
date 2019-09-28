ad8eed9dd59a33e4ce7b551359e3bb24b44aa68d
Allison Geismar <ageismar213@gmail.com>
2019-09-28 09:53:35 -0400

    made some changes

--- app.js
+++ app.js
@@ -9,44 +9,45 @@ const { db } = require('./models');
 
 const models = require('./models');
 
-
-db.authenticate().
-then(() => {
+db.authenticate().then(() => {
   console.log('connected to the database');
-})
+});
 
 //use this only for simple directories
 // app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
 //we have multiple folders, so we need the whole path.
 //__dirname says current directory of the file and wd add /public to it.
 
-
-app.use(express.urlencoded({extended: false}));
-app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
-  res.send(layout(" "));
+  res.send(layout(' '));
   //anchors have blue text
   // res.send(layout("<a>test</a>"));
-})
+});
 
+//create a route to get all buns
+
+//create a route to get all pups
+
+//create a route to get one bun
+
+//create a route to make one pup
 
 const init = async () => {
   // await models.User.sync()
   // await models.Page.sync()
-//or
-await models.db.sync()
+  //or
+  await models.db.sync();
 
-app.listen(PORT, function() {
-  console.log(`App is listening on port ${PORT}!`);
-});
-}
+  app.listen(PORT, function() {
+    console.log(`App is listening on port ${PORT}!`);
+  });
+};
 // this drops all tables then recreates them based on our JS definitions
-models.db.sync({force: true})
+models.db.sync({ force: true });
 
 init();
-
-
-
-
--- models/index.js
+++ models/index.js
@@ -3,16 +3,15 @@ const Sequelize = require('sequelize');
 
 //or id we do not want to see  the console output (SQL command text for each query) the we do this =>
 const db = new Sequelize('postgres://localhost:5432/Bunnies-Puppies', {
-    logging: false
+  logging: false
 });
 
-module.exports = {
-  db
-}
-//create models for puppies 
-  //include: name(string), age(int), breed(ENUM), isGoodBoy(Boolean) with default to true
+//create models for puppies
+//include: name(string), age(int), breed(ENUM), isGoodBoy(Boolean) with default to true
+
+const Bunnies = db.define('bunny', {});
 
 //create models for bunnies
-  //include: name(string), age(int), floppyEars(boolean)
+//include: name(string), age(int), floppyEars(boolean)
 
-  module.eports = { Bunnies, Puppies };
+module.exports = { db, Bunnies };
